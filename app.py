0fd61756c761a725d4a80012a971672296548fe1
Charlize Parkinson <charparkinson17@icloud.com>
2026-02-17 18:28:42 +0000

    updated naming for ingredients router to avoid confusion later

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from routers.ingredients import router
+from routers.ingredients import router as ingredients_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(ingredients_router)
 
 """
 uvicorn app:app --reload
