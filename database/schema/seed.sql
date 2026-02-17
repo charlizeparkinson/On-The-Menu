538372a39dbf764d207ff08bf85957fecf6d4f7a
Charlize Parkinson <charparkinson17@icloud.com>
2026-02-17 16:23:34 +0000

    adding seed data

---
+++ database/schema/seed.sql
@@ -0,0 +1,15 @@
+USE food_waste;
+
+INSERT INTO ingredients
+(ingredient_name, ingredient_type, ingredient_quantity, unit_name, expiration_date)
+VALUES
+('milk', 'Dairy', 2, 'litres', '2026-02-20'),
+('chicken breast', 'Protein', 500, 'grams', '2026-02-18'),
+('rice', 'Carbohydrate', 1, 'kg', '2026-06-01'),
+('spinach', 'Vegetable', 150, 'grams', '2026-02-17'),
+('tomatoes', 'Vegetable', 500, 'grams', '2026-02-19'),
+('eggs', 'Protein', 12, 'pieces', '2026-02-25'),
+('yogurt', 'Dairy', 750, 'ml', '2026-02-16'),
+('garlic', 'Seasoning', 3, 'cloves', '2026-03-10'),
+('onions', 'Vegetable', 5, 'pieces', '2026-03-01'),
+('cheddar cheese', 'Dairy', 350, 'grams', '2026-02-22');
